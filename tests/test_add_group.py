0c60e96eef8222f13ae60e9c4de4b73845900344
spottybug <evgeenia@gmail.com>
2020-12-23 00:07:13 +0300

    Добавление второго теста

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -59,6 +59,17 @@ class UntitledTestCase(unittest.TestCase):
         self.return_to_groups_page(wd)
         self.logout(wd)
 
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.init_group_creation(wd)
+        self.fill_group_form(wd, name="", header="", footer="")
+        self.submit_group_creation(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
     def tearDown(self):
         self.wd.quit()
 
