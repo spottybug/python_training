d33ca2ff492e9833fdd7f6bfe211445897fd89e7
spottybug <evgeenia@gmail.com>
2020-12-22 23:50:36 +0300

    Передача параметров username и password

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -14,12 +14,12 @@ class UntitledTestCase(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/index.php")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys("%s" % username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys("%s" % password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_groups_page(self, wd):
@@ -51,7 +51,7 @@ class UntitledTestCase(unittest.TestCase):
     def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
         self.init_group_creation(wd)
         self.fill_group_form(wd)
