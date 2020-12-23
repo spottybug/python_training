0afe24b5d9923b472a603839b1f9ece48a46d8cb
spottybug <evgeenia@gmail.com>
2020-12-23 23:37:21 +0300

    Добавлен файл Application

---
+++ tests/application.py
@@ -0,0 +1,72 @@
+# from selenium.webdriver.firefox.webdriver import WebDriver
+# from selenium import webdriver
+#browser = webdriver.Firefox(executable_path = '/Users/alex/anaconda3/bin/geckodriver')
+
+
+#from selenium.webdriver.firefox.webdriver import WebDriver
+import selenium.webdriver.remote
+from selenium.webdriver.firefox import webdriver
+from selenium import webdriver
+from selenium.webdriver.remote import webdriver
+
+
+class Application:
+    def __init__(self, driver: webdriver.WebDriver):
+        """
+        :type driver: this is browser driver (fro example, chrome-driver, firefox driver, etc...)
+        """
+        #self.wd = WebDriver()
+        self.wd = driver
+        self.wd.implicitly_wait(60)
+
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/index.php")
+
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("%s" % username)
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("%s" % password)
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("groups").click()
+
+    def init_group_creation(self):
+        wd = self.wd
+        wd.find_element_by_name("new").click()
+
+    def fill_group_form(self, group):
+        wd = self.wd
+        self.open_groups_page()
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("%s" % group.name)
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("%s" % group.header)
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("%s" % group.footer)
+        self.return_to_groups_page()
+
+    def submit_group_creation(self):
+        wd = self.wd
+        wd.find_element_by_name("submit").click()
+
+    def return_to_groups_page(self):
+        wd = self.wd
+        wd.find_element_by_link_text("group page").click()
+
+    def logout(self):
+        wd = self.wd
+        wd.find_element_by_link_text("Logout").click()
+
+    def destroy(self):
+        self.wd.quit()
