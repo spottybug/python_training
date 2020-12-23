3430a00fe718ef351a6a18f846dae767e9220d48
spottybug <evgeenia@gmail.com>
2020-12-23 18:03:01 +0300

    Добавлены дополнительные метода

--- tests/new_contact.py
+++ tests/new_contact.py
@@ -14,21 +14,21 @@ class test_new_contact(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(30)
-        self.base_url = "https://www.google.com/"
 
-    def test_untitled_test_case(self):
-        wd = self.wd
-        # open_home_page
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/edit.php")
-        # login
+
+    def login(self, wd):
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_id("LoginForm").submit()
-        # open_new_contact_form
+
+    def open_new_contact_form(self, wd):
         wd.find_element_by_name("firstname").clear()
-        # fill_new_contact_form
+
+    def fill_new_contact_form(self, wd):
         wd.find_element_by_name("firstname").send_keys("John")
         wd.find_element_by_name("middlename").clear()
         wd.find_element_by_name("middlename").send_keys("James")
@@ -58,12 +58,25 @@ class test_new_contact(unittest.TestCase):
         Select(wd.find_element_by_name("bmonth")).select_by_visible_text("January")
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys("1982")
-        # submit_creation
+
+    def submit_creation(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        # back_to_home_page
+
+    def back_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
-        # logout
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
+
+    def test_untitled_test_case(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_new_contact_form(wd)
+        self.fill_new_contact_form(wd)
+        self.submit_creation(wd)
+        self.back_to_home_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
